cdc6c4620a127f0e506c16038f73cdfb3ee1c84d
Javier Bull <javierbullreyes@gmail.com>
2018-04-20 04:37:14 -0500

    formulario

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-// alert("Hola pendejos");
+
+// alert("hola");
+function validar() {
+
+  var username = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var contraseña = document.getElementById('contraseña').value;
+  var ccontraseña = document.getElementById('confirmar').value;
+
+  if (username == "") {
+    alert(" Escriba su nombre");
+    return false;
+  }
+
+  if (email == "") {
+    alert(" Escriba su correo");
+    return false;
+  }
+
+  if (contraseña == "") {
+    alert(" Escriba su contraseña");
+    return false;
+  }
+
+  if (ccontraseña == "") {
+    alert(" vuelva a escribir su contraseña");
+    return false;
+  }
+
+}
